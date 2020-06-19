6194ef2442d1d7f04c2000ce7e094ef26f6af70a
Lee <zealotzergling@gmail.com>
2020-06-19 12:40:08 -0400

    pybot

--- eunjibot.py
+++ eunjibot.py
@@ -30,91 +30,17 @@ async def on_ready():
 #     if filename.endswith('.py'):
 #         client.load_extension(f'cogs.{filename[:-3]}')
 
-@client.command(aliases=['memes'])
-async def _memes(ctx):
-    responses = [
-        'https://i.imgur.com/JncHCj9.jpg', #weird insta meme
-        'https://imgur.com/FCNVmW6', #maggie's face
-        'https://imgur.com/p4pw1tg', #pumpkin
-        'https://imgur.com/iMJcc3A', #black maggie
-        'https://imgur.com/CxgwHYN', #dabb maggie
-        'https://imgur.com/Bt35YW0', #baby yoda
-        'https://imgur.com/OENkvsx', #worms
-        'https://imgur.com/adRS9G5', #trump
-        'https://imgur.com/aPdT94e', #feelsweirdmansnapchat
-        'https://imgur.com/Y2jv5OZ', #oddguy
-        'https://imgur.com/3qOIIsm', #nancy
-        'https://imgur.com/lytOnrm', #nancystare
-        'https://imgur.com/uWXBfAS', #monies
-        'https://imgur.com/rSCAiTy', #bigbois
-    ]
-    await ctx.send(random.choice(responses))
 
-@client.command(aliases=['iconic'])
-async def _iconic(ctx):
-    responses = [
-        'https://imgur.com/3DTXTQX',
-        'https://cdn.discordapp.com/attachments/700882681545883650/707027412311081080/image2.jpg',
-        'https://cdn.discordapp.com/attachments/700882681545883650/707027411937787955/image1.jpg',
-    ]
-    await ctx.send(random.choice(responses))
-
-@client.command(aliases=['maggie'])
-async def _maggie(ctx):
-    responses = [
-        'https://imgur.com/FCNVmW6', #maggie's face
-        'https://imgur.com/iMJcc3A', #black maggie
-        'https://imgur.com/CxgwHYN', #dabb maggie
-        'https://imgur.com/OENkvsx', #worms
-        'https://imgur.com/uWXBfAS', #monies
-        'https://cdn.discordapp.com/attachments/707280448858095716/707468264178974771/image0-9.png' #snow
-    ]
-    await ctx.send(random.choice(responses))
-
-#formatting for Emoji - Member Ping - Role Ping
-# <:emote_name:emote_ID>
-# <@!user_ID>
-# <@&role_ID>
 
 @client.command(aliases=['goodnight'])
 async def _goodnight(ctx):
-    # id = discord.Client(get_guild())
-    channels = ['general']
-    if ctx in channels:
-        iuwu = [
-            'https://tenor.com/view/iu-cute-hug-sleeping-gif-15049574',
-            'https://tenor.com/view/iu-sleep-sleepy-sleeping-tired-gif-12177560'
-        ]
-        await ctx.send(random.choice(iuwu))
 
-@client.command(aliases=['emb1'])
-async def _emb1(ctx):
-    embed = discord.Embed(
-        title = 'Jeong Eunji',
-        description = 'Best Singer',
-        color = 0xFF86E4
-    )
-    embed.set_author(name='Apink', icon_url='https://i.pinimg.com/originals/4b/4b/b7/4b4bb7a19133e996ff913e62ae43ca9f.jpg')
-    embed.set_footer(text='Copyright JamJam 2020', icon_url='')
-    embed.set_image(url='http://images6.fanpop.com/image/photos/39900000/-Jung-Eun-Ji-jung-eunji-39944017-540-810.jpg')
-    embed.set_thumbnail(url='https://images-na.ssl-images-amazon.com/images/I/41laNGJDa3L.jpg')
-    await ctx.send(embed=embed)
+    iuwu = [
+        'https://tenor.com/view/iu-cute-hug-sleeping-gif-15049574',
+        'https://tenor.com/view/iu-sleep-sleepy-sleeping-tired-gif-12177560'
+    ]
+    await ctx.send(random.choice(iuwu))
 
-@client.command(aliases=['form1'])
-async def _form1(ctx):
-    embed = discord.Embed(
-        title = 'Bitch Form',
-        description = 'Fill this out if you or someone else is feeling bitchy',
-        color = 0xEAFB19
-    )
-    file = discord.File('./forms/0form1.jpg')
-    embed.set_image(url='attachment://0form1.jpg')
-    await ctx.send(file=file, embed=embed)
-
-# @tasks.loop(seconds=10)
-# async def change_status():
-#     status = cycle(['Playing with JamJam', 'Sleepy Time'])
-#     await client.change_presence(activity=discord.Game(next(status)))
 
 @client.command(aliases=['whatslunch'])
 async def _whatslunch(ctx):
@@ -132,20 +58,7 @@ async def _whatslunch(ctx):
 async def _ban(ctx):
     await ctx.send('Please add a user ID in order to execute the command')
 
-@client.command(aliases=['latestcb'])
-async def _latestcb(ctx):
-    embed_IU = discord.Embed(
-        title = 'IU - Lee Ji-Eun (이지은)',
-        color = 0x4DFF29
-    )
 
-    embed_IU.set_thumbnail(url='https://i.imgur.com/M9p3WgK.jpg')
-    embed_IU.set_image(url='https://channel-korea.com/wp-content/uploads/2017/09/IU_1476317492_af_org-e1506689157858.jpg')
-    embed_IU.add_field(name='Member Profile', value='https://kprofiles.com/iu-profile/', inline=False)
-
-    embed_IU.add_field(name='Latest Comeback',value='https://www.youtube.com/watch?v=TgOu00Mf3kI', inline=False)
-
-    await ctx.send(embed=embed_IU)
 
 @client.command(aliases=['apink'])
 async def _apink(ctx, *, msg):
@@ -226,12 +139,7 @@ async def _izone(ctx, *, msg):
         await ctx.send(embed=embed_kp)
 
 
-@client.command(aliases=['supersecretcommandthatnobodywillknowbecauseitssolong'])
-async def _supersecretcommandthatnobodywillknowbecauseitssolong(ctx):
-    await ctx.send("Eunji ultimate bias 5 years poggers")
 
-# @client.command(aliases=['opggteam'])
-# async def _opggteam(ctx, *, msg):
 
 @client.event
 async def on_message(message):
@@ -275,7 +183,5 @@ async def _helpme(ctx):
     embed.add_field(name='goodnight', value='Sends you 1 out of 2 goodnight gif', inline=False)
     embed.add_field(name='latestcb', value='Sends the latest girl/girl group comeback', inline=False)
     await ctx.send(embed=embed)
-
-
 
 client.run(token)
