99d7665b7eba4f44677b45f162755bbc36c4cad9
jhakezebedee <jhake.aquino123@gmail.com>
2020-06-23 20:07:09 +0800

    Add Lee's weather function as a Cog

---
+++ cogs/weather.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+
+from secret_keys import OPENWEATHER_API_KEY
+
+class Weather(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def weather(self, ctx, *, msg):
+
+        sg = str(msg)
+
+        city = sg.split()
+
+        if len(city) > 1:
+            city = sg.replace(' ', '&')
+
+        elif len(city) == 1:
+            city = ''.join(msg)
+
+        else:
+            print("you're ruining the purpose of this smh put in something")
+
+        url =  f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}'
+
+        data = requests.get(url).json()
+
+        weather = data['weather'][0]['main'] #weather
+        weatherdesc = data['weather'][0]['description'] #weather description
+        tempk = data['main']['temp'] # temperature in kelvin
+        tempfs = data['main']['feels_like'] # feelslike temperature in kelvin
+        humidity = data['main']['humidity']
+
+        humidity = str(humidity) + '%'
+
+        #conversion from kelvin into fahrenheit
+        tempf = (tempk - 273.15) * 1.8 + 32 
+        tempfeels = (tempfs - 273.15) * 1.8 + 32 
+        
+        carrot = round(tempf, 1), round(tempfeels, 1), weather,weatherdesc, humidity
+
+        await ctx.send(carrot)
--- eunjibot.py
+++ eunjibot.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 from cogs.management import Management
 from cogs.lastfm import LastFM
 from cogs.misc import Misc
+from cogs.weather import Weather
 
 from secret_keys import DISCORD_BOT_TOKEN
 
@@ -12,5 +13,6 @@ client = commands.Bot(command_prefix = '~', activity = discord.Game("Eunji bot u
 client.add_cog(Management(client))
 client.add_cog(LastFM(client))
 client.add_cog(Misc(client))
+client.add_cog(Weather(client))
 
 client.run(DISCORD_BOT_TOKEN)
