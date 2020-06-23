80d62a277bb5ef8ab42bc673cf9e76a1a8fb424a
jhakezebedee <jhake.aquino123@gmail.com>
2020-06-23 20:06:54 +0800

    secret_keys: added openweather api key

--- secret_keys.py
+++ secret_keys.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+MONGODB_CONNECTION = os.getenv('MONGODB_CONNECTION')
+
 LF_API_KEY = os.getenv('LF_API_KEY')
 LF_API_SECRET = os.getenv('LF_API_SECRET')
-MONGODB_CONNECTION = os.getenv('MONGODB_CONNECTION')
+
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
