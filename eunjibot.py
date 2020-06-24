7c42c96794a06baa254b44c0fceaf89f79520fef
jhakezebedee <jhake.aquino123@gmail.com>
2020-06-25 03:20:56 +0800

    eunjibot: add the guildsettings cog. replace the on_messaage coroutine of the client
    Coroutine was replaced to override when the process_commands is called

--- eunjibot.py
+++ eunjibot.py
@@ -5,14 +5,23 @@ from cogs.management import ManagementCog
 from cogs.lastfm import LastFmCog
 from cogs.misc import MiscCog
 from cogs.weather import WeatherCog
+from cogs.guild_settings import GuildSettingsCog
+
+from models.guild_settings import AllGuildSettingsModel
+settings = AllGuildSettingsModel()
 
 from secret_keys import DISCORD_BOT_TOKEN
 
+async def blank(*args):
+    pass
+
 client = commands.Bot(command_prefix = '~', activity = discord.Game("Eunji bot under development"))
+client.on_message = blank
 
 client.add_cog(ManagementCog(client))
 client.add_cog(LastFmCog(client))
 client.add_cog(MiscCog(client))
 client.add_cog(WeatherCog(client))
+client.add_cog(GuildSettingsCog(client))
 
 client.run(DISCORD_BOT_TOKEN)
