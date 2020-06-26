8da3ddf9fd6915147f1c5ac8a9822a96ea8cfb7e
jhakezebedee <jhake.aquino123@gmail.com>
2020-06-27 03:37:33 +0800

    guild_settings: add support for new entries

--- models/guild_settings.py
+++ models/guild_settings.py
@@ -5,23 +5,37 @@ guilds = myclient.overall.guilds
 
 class AllGuildSettingsModel():
     def __init__(self):
-        pass
+        self._default = {
+            '_id' : None,
+            'prefix' : '~'
+        }
 
     def get_guild_settings(self, id):
         print(id)
-        return guilds.find_one({'id': f'{id}'})
+        return guilds.find_one({'_id': f'{id}'})
 
     def set_guild_settings(self, id, **kwargs):
-        new_settings = self.get_guild_settings(id)
 
-        print(new_settings)
-        print(kwargs)
+        new_settings = self.get_guild_settings(id)
+        new_entry = False
+
+        if new_settings == None:
+            new_entry = True
+            new_settings = self._default
+            new_settings['_id'] = str(id)
+
         for key in kwargs.keys():
             new_settings[key] = kwargs[key]
 
-        try:
-            myquery = { 'id': f'{id}' }
-            newvalues = { '$set':  new_settings }
-            guilds.update_one(myquery, newvalues)
-        except Exception as e:
-            print(e)
+        if new_entry:
+            try:
+                guilds.insert_one(new_settings)
+            except Exception as e:
+                print(e)
+        else:
+            try:
+                myquery = { '_id': f'{id}' }
+                newvalues = { '$set':  new_settings }
+                guilds.update_one(myquery, newvalues)
+            except Exception as e:
+                print(e)
