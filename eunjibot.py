1367880f6ba37d717fed5fae0fdbee688a8f3fe8
jhakezebedee <jhake.aquino123@gmail.com>
2020-06-30 11:31:40 +0800

    eunjibot: disable instagram again. add typing for evey command. make commands case insensitive

--- eunjibot.py
+++ eunjibot.py
@@ -16,15 +16,22 @@ from secret_keys import DISCORD_BOT_TOKEN
 async def blank(*args):
     pass
 
-client = commands.Bot(command_prefix = '~', activity = discord.Game("Eunji bot under development"))
+client = commands.Bot(command_prefix = '~', activity = discord.Game("Eunji bot under development"), case_insensitive=True)
 client.on_message = blank
 
 client.add_cog(ManagementCog(client))
 client.add_cog(LastFmCog(client))
 client.add_cog(MiscCog(client))
 client.add_cog(WeatherCog(client))
-client.add_cog(InstagramCog(client))
+#client.add_cog(InstagramCog(client))
 
 client.add_cog(GuildSettingsCog(client))
+
+@client.before_invoke
+async def before_any_command(ctx):
+    try:
+        await ctx.trigger_typing()
+    except:
+        pass
 
 client.run(DISCORD_BOT_TOKEN)
