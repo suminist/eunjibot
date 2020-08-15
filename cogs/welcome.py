c54c9ccc423da0ecde0f261db6be02f26f4f6bd3
minjuonlee <zealotzergling@gmail.com>
2020-08-15 19:52:50 -0400

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -28,15 +28,15 @@ class WelcomeCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel_id = await guilds.db_get_welcome_channel_id(member.guild.id)
+        # channel_id = await guilds.db_get_welcome_channel_id(member.guild.id)
+        channel = 734209655550509196
+        # if channel_id is None:
+        #     return
 
-        if channel_id is None:
-            return
-
-        channel = member.guild.get_channel(channel_id)
-        if channel is None:
-            print(f"Cannot welcome {member} to {member.guild.name} (invalid channel)")
-            return
+        # channel = member.guild.get_channel(channel_id)
+        # if channel is None:
+        #     print(f"Cannot welcome {member} to {member.guild.name} (invalid channel)")
+        #     return
 
         embed = discord.Embed(
 	        title="New Member Join",
@@ -45,6 +45,6 @@ class WelcomeCog(commands.Cog):
 	    embed.add_field(name=f"New member Join", value=f"Welcome to **{member.guild.name}**, {member.mention}. Recieve your roles in <#743300092907225168> and read the rules in <#743307491290382416>")
 	    embed.set_thumbnail(url=member.avatar_url)
 	    embed.set_footer(text=f"Member #**{len(guild.members)}**")
-	    await channel_id.send(embed=embed)
+	    # await channel_id.send(embed=embed)
 
         await channel.send(embed=embed)
