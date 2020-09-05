a935b1a2ef4ccd0e2ae12d47fd4bc188b30eba09
jhakezebedee <jhake.aquino123@gmail.com>
2020-09-06 01:39:25 +0800

    Implement dynamic welcome message

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,11 +7,16 @@ class WelcomeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group(case_insensitive=True)
     @commands.guild_only()
     @commands.bot_has_guild_permissions(ban_members=True)
     @commands.has_guild_permissions(ban_members=True)
-    async def welcome(self, ctx, *args):
+    async def welcome(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Add more arguments')
+
+    @welcome.command()
+    async def channel(self, ctx, *args):
         if len(args) == 0:
             await ctx.send("Please include channel")
             return
@@ -25,39 +30,91 @@ class WelcomeCog(commands.Cog):
         await guilds.db_set_welcome_channel_id(ctx.guild.id, channel.id)
         await ctx.send(f'Welcome channel is set to {channel.mention}.')
 
+    @welcome.command()
+    async def content(self, ctx):
+        print(ctx.message.content)
+        content = " ".join(ctx.message.content.split(" ")[2:])
+        print(content)
+
+        if content == "":
+            await ctx.send("Please include the content")
+            return
+
+        await guilds.db_set_welcome_content(ctx.guild.id, content)
+        embed = generateEmbed(
+                ctx.author,
+                await guilds.db_get_welcome_content(ctx.guild.id),
+                await guilds.db_get_welcome_image_url(ctx.guild.id)
+            )
+
+        await ctx.send(embed=embed)
+
+    @welcome.command()
+    async def image(self, ctx, *args):
+        image_url = " ".join(ctx.message.content.split(" ")[2:])
+
+        if image_url == "":
+            await ctx.send("Please include the image url")
+            return
+
+        await guilds.db_set_welcome_image_url(ctx.guild.id, image_url)
+        embed = generateEmbed(
+                ctx.author,
+                await guilds.db_get_welcome_content(ctx.guild.id),
+                await guilds.db_get_welcome_image_url(ctx.guild.id)
+            )
+
+        try:
+            await ctx.send(embed=embed)
+        except Exception as e:
+            print(e)
+            await guilds.db_set_welcome_image_url(ctx.guild.id, None)
+            await ctx.send("Invalid image URL")
+
+    @welcome.command()
+    async def preview(self, ctx):
+        embed = generateEmbed(
+                ctx.author,
+                await guilds.db_get_welcome_content(ctx.guild.id),
+                await guilds.db_get_welcome_image_url(ctx.guild.id)
+            )
+
+        await ctx.send(
+            f"Welcome channel in <#{await guilds.db_get_welcome_channel_id(ctx.guild.id)}>",
+            embed=embed)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        #channel_id = await guilds.db_get_welcome_channel_id(member.guild.id)
+        channel_id = await guilds.db_get_welcome_channel_id(member.guild.id)
 
-        #if channel_id is None:
-        #    return
+        if channel_id is None:
+            return
 
-        #channel = member.guild.get_channel(channel_id)
-        #if channel is None:
-        #    print(f"Cannot welcome {member} to {member.guild.name} (invalid channel)")
-        #    return
+        channel = member.guild.get_channel(channel_id)
+        if channel is None:
+            print(f"Cannot welcome {member} to {member.guild.name} (invalid channel)")
+            return
 
-        #await channel.send(f"Welcome to {member.guild.name}, {member.mention}!")
+        embed = generateEmbed(
+                member,
+                await guilds.db_get_welcome_content(member.guild.id),
+                await guilds.db_get_welcome_image_url(member.guild.id)
+            )
 
-        if(member.guild.id == 661452294822559805):
-            channel = member.guild.get_channel(661825279592759327)
+        await channel.send(embed=embed)
 
-            embed = discord.Embed(color=0xFE7DFA)
-            embed.add_field(name='New Member Join', value=f"Welcome to {member.guild.name}, {member.mention}. Lee is uwu")
-            embed.set_thumbnail(url=member.avatar_url)
-            embed.set_image(url='https://cdn.discordapp.com/attachments/748678569689088020/748682063892185180/EexCBsuUwAArzoQ.jpg')
-            embed.set_footer(text=f"Member #**{len(member.guild.members)}**")
 
-            await channel.send(embed=embed)
-        
-        else:
-            channel = member.guild.get_channel(734209655550509196)
+def generateEmbed(member, content, image_url):
+    if content is None:
+        content = "Welcome to {guild}, {member}"
 
-            embed = discord.Embed(color=0xFE7DFA)
-            embed.add_field(name='New Member Join', value=f"Welcome to {member.guild.name}, {member.mention}. recieve your roles in <#743300092907225168> and read the rules in <#743307491290382416>")
-            embed.set_thumbnail(url=member.avatar_url)
-            embed.set_image(url='https://i.imgur.com/Yfaat1t.png')
-            embed.set_footer(text=f"Member #**{len(member.guild.members)}**")
+    content = content.replace("{guild}", member.guild.name)
+    content = content.replace("{member}", member.mention)
 
-            await channel.send(embed=embed)
+    embed = discord.Embed(color=0xFE7DFA, title='New Member Join', description=content)
+    embed.set_thumbnail(url=member.avatar_url)
+    if image_url is not None:
+        embed.set_image(url=image_url)
+    embed.set_footer(text=f"Member #{len(member.guild.members)}")
+
+    return embed
