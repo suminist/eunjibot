4f8da99825faee9ad32d923f1bf0a80625b29f5d
jhakezebedee <jhake.aquino123@gmail.com>
2020-09-06 13:59:37 +0800

    Reduce welcome to a single get/set function

--- db/guilds.py
+++ db/guilds.py
@@ -3,107 +3,86 @@ from db import db
 guilds_collection = db.get_guilds_collection()
 
 
-async def db_set_welcome_channel_id(guild_id, channel_id):
+async def db_set_welcome(
+            guild_id, *,
+            welcome_channel_id=None,
+            welcome_title=None,
+            welcome_content=None,
+            welcome_image_url=None
+        ):
     guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
 
     if guild_document is None:
-        await db_create_new_guild(guild_id, welcome_channel_id=channel_id)
+        await db_create_new_guild(
+            guild_id,
+            welcome_channel_id=welcome_channel_id,
+            welcome_title=welcome_title,
+            welcome_content=welcome_content,
+            welcome_image_url=welcome_image_url
+            )
         return
     else:
         myquery = {"_id": str(guild_id)}
         newvalues = guild_document
-        newvalues["welcomeChannelId"] = str(channel_id)
+        if welcome_channel_id is not None:
+            newvalues["welcomeChannelId"] = str(welcome_channel_id)
+
+        if welcome_title is not None:
+            newvalues["welcomeTitle"] = welcome_title
+
+        if welcome_content is not None:
+            newvalues["welcomeContent"] = welcome_content
+
+        if welcome_image_url is not None:
+            newvalues["welcomeImageUrl"] = welcome_image_url
+
         await guilds_collection.update_one(myquery, {"$set": newvalues})
         return
 
 
-async def db_get_welcome_channel_id(guild_id):
+async def db_get_welcome(guild_id):
     guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
     if guild_document is None:
         return None
+
+    welcome_info = {}
+
+    if "welcomeChannelId" in guild_document and guild_document["welcomeChannelId"] != "None":
+        welcome_info["channel_id"] = int(guild_document["welcomeChannelId"])
     else:
-        if "welcomeChannelId" in guild_document:
-            channel_id = guild_document["welcomeChannelId"]
-            if channel_id != "None":
-                return int(channel_id)
-            else:
-                return None
-        else:
-            await db_set_welcome_channel_id(guild_id, None)
-            return None
+        welcome_info["channel_id"] = None
 
-
-async def db_set_welcome_content(guild_id, content):
-    guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
-
-    if guild_document is None:
-        await db_create_new_guild(guild_id, welcome_content=content)
-        return
+    if "welcomeTitle" in guild_document and guild_document["welcomeTitle"] != "None":
+        welcome_info["title"] = guild_document["welcomeTitle"]
     else:
-        myquery = {"_id": str(guild_id)}
-        newvalues = guild_document
-        newvalues["welcomeContent"] = str(content)
-        await guilds_collection.update_one(myquery, {"$set": newvalues})
-        return
+        welcome_info["title"] = None
 
-
-async def db_get_welcome_content(guild_id):
-    guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
-    if guild_document is None:
-        return None
+    if "welcomeContent" in guild_document and guild_document["welcomeContent"] != "None":
+        welcome_info["content"] = guild_document["welcomeContent"]
     else:
-        if "welcomeContent" in guild_document:
-            content = guild_document["welcomeContent"]
-            if content != "None":
-                return content
-            else:
-                return None
-        else:
-            await db_set_welcome_content(guild_id, None)
-            return None
+        welcome_info["content"] = None
 
-
-async def db_set_welcome_image_url(guild_id, image_url):
-    guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
-
-    if guild_document is None:
-        await db_create_new_guild(guild_id, welcome_image_url=image_url)
-        return
+    if "welcomeImageUrl" in guild_document and guild_document["welcomeImageUrl"] != "None":
+        welcome_info["image_url"] = guild_document["welcomeImageUrl"]
     else:
-        myquery = {"_id": str(guild_id)}
-        newvalues = guild_document
-        newvalues["welcomeImageUrl"] = str(image_url)
-        await guilds_collection.update_one(myquery, {"$set": newvalues})
-        return
+        welcome_info["image_url"] = None
 
-
-async def db_get_welcome_image_url(guild_id):
-    guild_document = await guilds_collection.find_one({"_id": str(guild_id)})
-    if guild_document is None:
-        return None
-    else:
-        if "welcomeImageUrl" in guild_document:
-            image_url = guild_document["welcomeImageUrl"]
-            if image_url != "None":
-                return image_url
-            else:
-                return None
-        else:
-            await db_set_welcome_image_url(guild_id, None)
-            return None
+    return welcome_info
 
 
 async def db_create_new_guild(
-        guild_id, *,
-        prefix=None,
-        welcome_channel_id=None,
-        welcome_content=None,
-        welcome_image_url=None
-    ):
+            guild_id, *,
+            prefix=None,
+            welcome_channel_id=None,
+            welcome_title=None,
+            welcome_content=None,
+            welcome_image_url=None
+        ):
     newvalues = {
         "_id":                  str(guild_id),
         "prefix":               prefix,
         "welcomeChannelId":     str(welcome_channel_id),
+        "welcomeTitle":         str(welcome_title),
         "welcomeContent":       str(welcome_content),
         "welcomeImageUrl":      str(welcome_image_url)
     }
