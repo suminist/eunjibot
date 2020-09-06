0ad1505b9f6305bc8e130e68258c8c6fecbc2c11
jhakezebedee <jhake.aquino123@gmail.com>
2020-09-06 23:03:02 +0800

    Use the new db functions

--- cogs/guild_settings.py
+++ cogs/guild_settings.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-from models.guild_settings import AllGuildSettingsModel
+from db import guilds
 
-settings = AllGuildSettingsModel()
 
 class GuildSettingsCog(commands.Cog):
     def __init__(self, bot):
@@ -14,21 +13,23 @@ class GuildSettingsCog(commands.Cog):
         if len(args) == 0:
             await ctx.send('Please enter the new prefix.')
             return
-    
+
         new_prefix = args[0]
         guild_id = ctx.guild.id
-        settings.set_guild_settings(id=guild_id, prefix=new_prefix)
+        await guilds.db_set_prefix(guild_id, prefix=new_prefix)
 
         await ctx.send(f'Prefix for this server is set to {new_prefix}')
 
     @commands.Cog.listener()
     async def on_message(self, message):
         guild_id = message.guild.id
-        try:
-            prefix = settings.get_guild_settings(str(guild_id))['prefix']
-        except:
+
+        prefix = await guilds.db_get_prefix(guild_id)
+
+        if prefix is None:
             prefix = '~'
 
         if message.content.startswith(prefix):
             message.content = message.content.replace(prefix, '~', 1)
-            await self.bot.process_commands(message)
+        
+        await self.bot.process_commands(message)
