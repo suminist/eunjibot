3fac7d65d22a264bdc1aa07baacde091542c396b
jhakezebedee <jhake.aquino123@gmail.com>
2020-09-16 23:31:29 +0800

    Add description and usage for the commands. Move the modrole command to here

--- cogs/guild_settings.py
+++ cogs/guild_settings.py
@@ -4,10 +4,19 @@ from db import guilds
 
 
 class GuildSettingsCog(commands.Cog):
+    """
+    For guild settings.
+    """
     def __init__(self, bot):
         self.bot = bot
+        self.__cog_name__ = "GuildSettings"
 
-    @commands.command()
+    @commands.command(
+        description="Set prefix for this server",
+        usage="Args:\n" +
+              "- Prefix"
+    )
+    @commands.has_guild_permissions(administrator=True)
     @commands.guild_only()
     async def setprefix(self, ctx, *args):
         if len(args) == 0:
@@ -19,6 +28,34 @@ class GuildSettingsCog(commands.Cog):
         await guilds.db_set_prefix(guild_id, prefix=new_prefix)
 
         await ctx.send(f'Prefix for this server is set to {new_prefix}')
+
+    @commands.command(
+        description="Set the modrole for this server. This will allow members with role to do some commands, like twitter and welcome commands.",
+        usage="Args:\n" +
+              "- Role"
+    )
+    @commands.guild_only()
+    @commands.has_guild_permissions(administrator=True)
+    async def modrole(self, ctx, *args):
+        if len(args) == 0:
+            try:
+                moderator_role_id = await guilds.db_get_moderator_role_id(ctx.guild.id)
+                moderator_role = await commands.RoleConverter().convert(ctx, moderator_role_id)
+                await ctx.send(f"The current moderator role is {moderator_role.mention}")
+            except Exception as e:
+                await ctx.send("There is no moderator role")
+
+            return
+
+        try:
+            moderator_role = await commands.RoleConverter().convert(ctx, args[0])
+        except Exception as e:
+            print(e)
+            await ctx.send("Invalid role")
+            return
+
+        await guilds.db_set_moderator_role_id(ctx.guild.id, moderator_role.id)
+        await ctx.send(f"{moderator_role.mention} set as moderator role for this server.")
 
     @commands.Cog.listener()
     async def on_message(self, message):
