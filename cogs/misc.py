d76a73430bafa80f6eeaee124c05035355e1e158
jhake <73086064+jhake-papa@users.noreply.github.com>
2021-06-16 19:08:48 +0800

    update channel id for krupdate

--- cogs/misc.py
+++ cogs/misc.py
@@ -243,7 +243,7 @@ class MiscCog(commands.Cog):
             krbot = db['hyewonfragrant']
 
             channels = [self.bot.get_channel(
-                723401657148244009), self.bot.get_channel(743562872025514075), self.bot.get_channel(723574110143381534)]
+                723401657148244009), self.bot.get_channel(743562872025514075), self.bot.get_channel(564352023928242186)]
             url = 'https://www.koreanclass101.com/korean-phrases/'
             headers = {
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)Version/12.1.1 Safari/605.1.15'}
